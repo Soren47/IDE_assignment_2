e55a9d4b0090a030cc629c7d617a7c820861bf61
Soren47 <Ssp_47@yahoo.dk>
2017-12-09 14:08:04 +0100

    All pictures are right dimensions now!

--- trump_tree.js
+++ trump_tree.js
@@ -116,24 +116,46 @@ function init() {
 
 
     var allPhotoTexts = {
-                        "Ivana Trump" : "By Christopherpeterson at English Wikipedia, CC BY 3.0, https://commons.wikimedia.org/w/index.php?curid=3071583 - File:Ivana Trump.jpg, CC BY-SA 3.0, https://commons.wikimedia.org/w/index.php?curid=50250176",
-                        "Donald Trump" : "By Shealah Craighead - https://www.whitehouse.gov/the-press-office/2017/10/31/white-house-releases-official-portraits-president-donald-j-trump-and, Public Domain, https://commons.wikimedia.org/w/index.php?curid=63768460",
-                        "Donald Trump Jr." : "By Gage Skidmore, CC BY-SA 2.0, https://commons.wikimedia.org/w/index.php?curid=52590948",
-                        "Donald Trump Jr._family" : "Jen Blakele/Courtesy of Trump family",
-                        "Vanessa Trump": "Getty Images",
-                        "Ivanka Trump" : "By Michael Vadon - Own work, CC BY-SA 4.0, https://commons.wikimedia.org/w/index.php?curid=63759407",
-                        "Jared Kushner" : "By DoD Photo by Navy Petty Officer 2nd Class Dominique A. Pineiro, Office of the Chairman of the Joint Chiefs of Staff for Public Affairs - https://www.flickr.com/photos/thejointstaff/33806148366/, CC BY 2.0, https://commons.wikimedia.org/w/index.php?curid=61287038",
-                        "Eric Trump" : "By Ali Shaker/VOA - http://m.voanews.com/a/photo-gallery-republican-national-convention-night-3/3428570.html, Public Domain, https://commons.wikimedia.org/w/index.php?curid=50358094",
-                        "Lara Yunaska" : "By Rachel Larue - https://www.dvidshub.net/image/3108118/president-elect-donald-j-trump-and-vice-president-elect-mike-pence-place-wreath-tomb-unknown, Public Domain, https://commons.wikimedia.org/w/index.php?curid=60203259",
-                        "Tiffany Trump" : "By Ali Shaker/VOA - http://m.voanews.com/a/republican-national-convention/3426618.html, Public Domain, https://commons.wikimedia.org/w/index.php?curid=50358143",
-                        "Barron Trump" : "Getty Images",
-                        "Ivanka Trump_family" : "Joe Raedle/Getty Images",
-                        "Marla Maples" : "By lukeford.net - http://www.lukeford.net/Images/photos4/071205/68.htm, CC BY 2.5, https://commons.wikimedia.org/w/index.php?curid=4292871",
-                        "Melania Trump" : "By Regine MahauxWeaver, Hilary (3 April 2017). Here's What You Should Know About Melania Trump's Official First Lady Portrait. Vanity Fair. Archived from the original on 7 April 2017. \"[O]n Monday[,] . . . the White House released [Melania Trump's] first official portrait, which was taken by Belgian photographer Regine Mahaux.\"Laurent, Olivier (4 April 2017). The Story Behind Melania Trump's Official Portrait. Time. Archived from the original on 7 April 2017. - whitehouse.gov/administration/first-lady-melania-trump (page) (archived)whitehouse.gov/sites/whitehouse.gov/files/images/flotus.png (direct link) (archived)whitehouse.gov/copyright (license) (archived), CC BY 3.0 us, https://commons.wikimedia.org/w/index.php?curid=57696382"
-                        };
+        "Ivana Trump": "By Christopherpeterson at English Wikipedia, CC BY 3.0, https://commons.wikimedia.org/w/index.php?curid=3071583 " +
+            "- File:Ivana Trump.jpg, CC BY-SA 3.0, https://commons.wikimedia.org/w/index.php?curid=50250176",
+
+        "Donald Trump": "By Shealah Craighead - https://www.whitehouse.gov/the-press-office/2017/10/31/white-house-releases-official-portraits-president-donald-j-trump-and," +
+            " Public Domain, https://commons.wikimedia.org/w/index.php?curid=63768460",
+
+        "Donald Trump Jr.": "By Gage Skidmore, CC BY-SA 2.0, https://commons.wikimedia.org/w/index.php?curid=52590948",
+
+        "Donald Trump Jr._family": "Jen Blakele/Courtesy of Trump family",
+
+        "Vanessa Trump": "Getty Images",
+
+        "Ivanka Trump": "By Michael Vadon - Own work, CC BY-SA 4.0, https://commons.wikimedia.org/w/index.php?curid=63759407",
+
+        "Jared Kushner": "By DoD Photo by Navy Petty Officer 2nd Class Dominique A. Pineiro, Office of the Chairman of the Joint" +
+            " Chiefs of Staff for Public Affairs - https://www.flickr.com/photos/thejointstaff/33806148366/, CC BY 2.0, https://commons.wikimedia.org/w/index.php?curid=61287038",
+
+        "Eric Trump": "By Ali Shaker/VOA - http://m.voanews.com/a/photo-gallery-republican-national-convention-night-3/3428570.html," +
+            " Public Domain, https://commons.wikimedia.org/w/index.php?curid=50358094",
+
+        "Lara Yunaska": "By Rachel Larue - https://www.dvidshub.net/image/3108118/president-elect-donald-j-trump-" +
+            "and-vice-president-elect-mike-pence-place-wreath-tomb-unknown, Public Domain, https://commons.wikimedia.org/w/index.php?curid=60203259",
+
+        "Tiffany Trump": "By Ali Shaker/VOA - http://m.voanews.com/a/republican-national-convention/3426618.html, Public Domain, https://commons.wikimedia.org/w/index.php?curid=50358143",
+
+        "Barron Trump": "Getty Images",
+
+        "Ivanka Trump_family": "Joe Raedle/Getty Images",
+
+        "Marla Maples": "By lukeford.net - http://www.lukeford.net/Images/photos4/071205/68.htm, CC BY 2.5, " +
+            "https://commons.wikimedia.org/w/index.php?curid=4292871",
+
+        "Melania Trump": "By Regine MahauxWeaver, Hilary (3 April 2017). Here's What You Should Know About Melania Trump's " +
+            "Official First Lady Portrait. Vanity Fair. Archived from the original on 7 April 2017."
+    };
 
     var photo = d3.select("body")
         .select("img");
+
+    d3.select("body").select("#nice").style("font", "20px sans-serif");
 
     dTree.init(treeData, {
         target: "#graph",
@@ -157,24 +179,46 @@ function init() {
                 }
 
 
-
                 var newPhotoText = allPhotoTexts[name];
                 //console.log(newPhotoText);
 
-                photo.attr("src", "ass3_images/" + name + ".jpg");
+                var photoName = "ass3_images/" + name + ".jpg";
+
+                photo.attr("src", photoName);
+
+                var newPhotoH = 0;
+                var newPhotoW = 0;
+
+                var img = document.getElementsByTagName("img")[0];
+
+                img.onload=function() {
+                    //console.log("Width",img.naturalWidth);
+                    //console.log("Height",img.naturalHeight);
+
+                    //console.log("new dimensions:");
+                    newPhotoH = 300;
+                    newPhotoW = 300 * ( img.naturalWidth / img.naturalHeight );
+
+                    //console.log("new H" + newPhotoH);
+                    //console.log("new W" + newPhotoW);
+                    //console.log(newPhotoH);
+                    //console.log(newPhotoW);
+
+                    photo.attr("src", photoName).attr("height", newPhotoH.toString()).attr("width", newPhotoW.toString());
+
+
+                };
+
+
+
+
 
                 d3.select("body").select("#photoText").text(newPhotoText);
-
-
-
 
 
             }
         }
     })
-
-
-
 
 
 };

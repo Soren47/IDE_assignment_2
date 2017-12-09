cdd94721d87e2e58c0101618e07e74318431ebb1
Ssp47 <ssp@cortilabs.com>
2017-12-09 12:22:47 +0100

    Added so that a picture and a text is shown when a name is pressed.

--- trump_tree.js
+++ trump_tree.js
@@ -112,17 +112,47 @@ function init() {
                 ]
             }
         ]
-    }]
+    }];
+
+
+    var allPhotoTexts = {"Ivana Trump" : "By Christopherpeterson at English Wikipedia, CC BY 3.0, https://commons.wikimedia.org/w/index.php?curid=3071583 - File:Ivana Trump.jpg, CC BY-SA 3.0, https://commons.wikimedia.org/w/index.php?curid=50250176",
+                         "Donald Trump" : "By Shealah Craighead - https://www.whitehouse.gov/the-press-office/2017/10/31/white-house-releases-official-portraits-president-donald-j-trump-and, Public Domain, https://commons.wikimedia.org/w/index.php?curid=63768460",
+                        "Donald Trump Jr." : "By Gage Skidmore, CC BY-SA 2.0, https://commons.wikimedia.org/w/index.php?curid=52590948",
+                        "b" : "c"
+
+                        };
+
+    var photo = d3.select("body")
+        .select("img");
 
     dTree.init(treeData, {
         target: "#graph",
         debug: true,
-        height: 800,
-        width: 1200,
+        height: 300,
+        width: 800,
         callbacks: {
             nodeClick: function (name, extra) {
                 console.log(name);
+
+
+
+                var newPhotoText = allPhotoTexts[name];
+                //console.log(newPhotoText);
+
+                photo.attr("src", "ass3_images/" + name + ".jpg");
+
+                d3.select("body").select("#photoText").text(newPhotoText);
+
+
+
+
+
             }
         }
     })
+
+
+
+
+
 };

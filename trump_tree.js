67c1d0541e35b2287dc5db42801ff1ea790d6408
ivbm <33903935+ivbm@users.noreply.github.com>
2017-12-12 19:22:50 +0100

    Update trump_tree.js

--- trump_tree.js
+++ trump_tree.js
@@ -117,10 +117,10 @@ function init() {
 
     var allPhotoTexts = {
         "Ivana Trump": "By Christopherpeterson at English Wikipedia, CC BY 3.0, https://commons.wikimedia.org/w/index.php?curid=3071583 " +
-            "- File:Ivana Trump.jpg, CC BY-SA 3.0, https://commons.wikimedia.org/w/index.php?curid=50250176",
+        "- File:Ivana Trump.jpg, CC BY-SA 3.0, https://commons.wikimedia.org/w/index.php?curid=50250176",
 
         "Donald Trump": "By Shealah Craighead - https://www.whitehouse.gov/the-press-office/2017/10/31/white-house-releases-official-portraits-president-donald-j-trump-and," +
-            " Public Domain, https://commons.wikimedia.org/w/index.php?curid=63768460",
+        " Public Domain, https://commons.wikimedia.org/w/index.php?curid=63768460",
 
         "Donald Trump Jr.": "By Gage Skidmore, CC BY-SA 2.0, https://commons.wikimedia.org/w/index.php?curid=52590948",
 
@@ -131,13 +131,13 @@ function init() {
         "Ivanka Trump": "By Michael Vadon - Own work, CC BY-SA 4.0, https://commons.wikimedia.org/w/index.php?curid=63759407",
 
         "Jared Kushner": "By DoD Photo by Navy Petty Officer 2nd Class Dominique A. Pineiro, Office of the Chairman of the Joint" +
-            " Chiefs of Staff for Public Affairs - https://www.flickr.com/photos/thejointstaff/33806148366/, CC BY 2.0, https://commons.wikimedia.org/w/index.php?curid=61287038",
+        " Chiefs of Staff for Public Affairs - https://www.flickr.com/photos/thejointstaff/33806148366/, CC BY 2.0, https://commons.wikimedia.org/w/index.php?curid=61287038",
 
         "Eric Trump": "By Ali Shaker/VOA - http://m.voanews.com/a/photo-gallery-republican-national-convention-night-3/3428570.html," +
-            " Public Domain, https://commons.wikimedia.org/w/index.php?curid=50358094",
+        " Public Domain, https://commons.wikimedia.org/w/index.php?curid=50358094",
 
         "Lara Yunaska": "By Rachel Larue - https://www.dvidshub.net/image/3108118/president-elect-donald-j-trump-" +
-            "and-vice-president-elect-mike-pence-place-wreath-tomb-unknown, Public Domain, https://commons.wikimedia.org/w/index.php?curid=60203259",
+        "and-vice-president-elect-mike-pence-place-wreath-tomb-unknown, Public Domain, https://commons.wikimedia.org/w/index.php?curid=60203259",
 
         "Tiffany Trump": "By Ali Shaker/VOA - http://m.voanews.com/a/republican-national-convention/3426618.html, Public Domain, https://commons.wikimedia.org/w/index.php?curid=50358143",
 
@@ -146,17 +146,14 @@ function init() {
         "Ivanka Trump_family": "Joe Raedle/Getty Images",
 
         "Marla Maples": "By lukeford.net - http://www.lukeford.net/Images/photos4/071205/68.htm, CC BY 2.5, " +
-            "https://commons.wikimedia.org/w/index.php?curid=4292871",
+        "https://commons.wikimedia.org/w/index.php?curid=4292871",
 
         "Melania Trump": "By Regine MahauxWeaver, Hilary (3 April 2017). Here's What You Should Know About Melania Trump's " +
-            "Official First Lady Portrait. Vanity Fair. Archived from the original on 7 April 2017."
+        "Official First Lady Portrait. Vanity Fair. Archived from the original on 7 April 2017."
     };
 
     var photo = d3.select("body")
         .select("img");
-
-    d3.select("body").select("#nice").style("font", "20px sans-serif");
-    d3.select("body").select("normalText").style("font", "10px sans-serif");
 
     dTree.init(treeData, {
         target: "#graph",
@@ -181,7 +178,6 @@ function init() {
 
 
                 var newPhotoText = allPhotoTexts[name];
-                //console.log(newPhotoText);
 
                 var photoName = "ass3_images/" + name + ".jpg";
 
@@ -193,29 +189,16 @@ function init() {
                 var img = document.getElementsByTagName("img")[0];
 
                 img.onload=function() {
-                    //console.log("Width",img.naturalWidth);
-                    //console.log("Height",img.naturalHeight);
 
-                    //console.log("new dimensions:");
                     newPhotoH = 300;
                     newPhotoW = 300 * ( img.naturalWidth / img.naturalHeight );
-
-                    //console.log("new H" + newPhotoH);
-                    //console.log("new W" + newPhotoW);
-                    //console.log(newPhotoH);
-                    //console.log(newPhotoW);
 
                     photo.attr("src", photoName).attr("height", newPhotoH.toString()).attr("width", newPhotoW.toString());
 
 
                 };
 
-
-
-
-
                 d3.select("body").select("#photoText").text(newPhotoText);
-
 
             }
         }

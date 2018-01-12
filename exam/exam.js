c8bbcfebafc857d0c9ebe7444970f0d31f42ce7c
mihaelabeatriceneagu <30812168+mihaelabeatriceneagu@users.noreply.github.com>
2018-01-12 18:23:29 +0100

    Update exam.js
    mercator layout

--- exam/exam.js
+++ exam/exam.js
@@ -3,8 +3,22 @@ d3.select(window).on('load', init);
 function init() {
 
 
+    var svg = d3.select("svg");
 
+    var projection = d3.geoMercator();
+    var path = d3.geoPath().projection(projection);
 
+    d3.json("https://unpkg.com/world-atlas@1/world/50m.json", function(error, world)
+    {
+        if (error) throw error;
+
+        svg.append("g")
+            .selectAll("path")
+            .data(topojson.feature(world, world.objects.countries).features)
+            .enter().append("path")
+            .attr("d", path);
+
+    });
 
 
 }

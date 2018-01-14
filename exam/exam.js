ee3b6aa7f2c5b82331b20dd603b34746467c1cff
Soren47 <Ssp_47@yahoo.dk>
2018-01-14 16:47:24 +0100

    just minor changes

--- exam/exam.js
+++ exam/exam.js
@@ -2,58 +2,58 @@ d3.select(window).on('load', init);
 
 function init() {
 
-var width = 800,
-    height = 500;
+    var width = 800,
+        height = 500;
 
-var projection = d3.geoMercator()
-    .center([50, 10]) //long and lat starting position
-    .scale(150) //starting zoom position
-    .rotate([10,0]); //where world split occurs
+    var projection = d3.geoMercator()
+        .center([50, 10]) //long and lat starting position
+        .scale(150) //starting zoom position
+        .rotate([10,0]); //where world split occurs
 
-var svg = d3.select("#worldMap")
-    .attr("width", width)
-    .attr("height", height);
+    var svg = d3.select("#worldMap")
+        .attr("width", width)
+        .attr("height", height);
 
-var path = d3.geoPath()
-    .projection(projection);
+    var path = d3.geoPath()
+        .projection(projection);
 
-//path
-var g = svg.append("g");
+    //path
+    var g = svg.append("g");
 
-// load and display the world and locations
-d3.json("https://gist.githubusercontent.com/d3noob/5193723/raw/world-110m2.json", function(error, topology) {
-    var world = g.selectAll("path")
-        .data(topojson.object(topology, topology.objects.countries).geometries)
-        .enter()
-        .append("path")
-        .attr("d", path)
+    // load and display the world and locations
+    d3.json("https://gist.githubusercontent.com/d3noob/5193723/raw/world-110m2.json", function(error, topology) {
+        var world = g.selectAll("path")
+            .data(topojson.object(topology, topology.objects.countries).geometries)
+            .enter()
+            .append("path")
+            .attr("d", path)
 
-});
-
-//zoom and pan functionality
-var zoom = d3.zoom()
-    .on("zoom",function() {
-        g.attr("transform","translate("+
-            d3.event.translate.join(",")+")scale("+d3.event.scale+")");
-        g.selectAll("path")
-            .attr("d", path.projection(projection));
     });
-svg.call(zoom);
 
-d3.csv("language.csv", function(d){
-    d.latitude = parseFloat(d.latitude)
-    d.longitude = parseFloat(d.longitude)
-    return d;
-}, function(data) {
-    //console.log(data);
-    //data[0]
-    var group= d3.nest()
-        .key(function(d){
-            return d.family
-        })
-        .sortKeys(d3.ascending)
-        .map(data)
-    console.log(group);
+    //zoom and pan functionality
+    var zoom = d3.zoom()
+        .on("zoom",function() {
+            g.attr("transform","translate("+
+                d3.event.translate.join(",")+")scale("+d3.event.scale+")");
+            g.selectAll("path")
+                .attr("d", path.projection(projection));
+        });
+    svg.call(zoom);
+
+    d3.csv("language.csv", function(d){
+        d.latitude = parseFloat(d.latitude);
+        d.longitude = parseFloat(d.longitude);
+        return d;
+    }, function(data) {
+        //console.log(data);
+        //data[0]
+        var group = d3.nest()
+            .key(function(d){
+                return d.family
+            })
+            .sortKeys(d3.ascending)
+            .map(data);
+        console.log(group);
 
     function draw_circles(filterdata){
         var circle = svg.selectAll("circle")
@@ -75,7 +75,9 @@ d3.csv("language.csv", function(d){
             .exit()
             .remove()
     }
-    draw_circles(data)
+
+
+    draw_circles(data);
     //console.log(projection([data[0].longitude, data[0].latitude]))
     //console.log(data[0])
 
@@ -87,7 +89,7 @@ d3.csv("language.csv", function(d){
         .append("option")
         .text(function (i) {
             return i.key;
-        })
+        });
 
     var selected_element = d3.select("#family")
         .on("change", function(){
